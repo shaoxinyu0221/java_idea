689a33c5d5ca2bef50154875405d3261d77a2555
shaoxinyu <2424590457@qq.com>
2022-05-10 18:43:38 +0800

    Initial commit

---
+++ jdbc_demo/src/com/shaoxinyu/lesson01/JdbcUtil.java
@@ -0,0 +1,14 @@
+package com.shaoxinyu.lesson01;
+
+import java.util.Properties;
+
+/**
+ * @author 邵鑫雨
+ */
+public class JdbcUtil {
+
+    public static void getConnection(){
+        Properties properties = new Properties();
+    }
+
+}
