cbe3b8731886165ce72e5dd75bb59aa0162db265
shaoxinyu <2424590457@qq.com>
2022-05-15 21:57:19 +0800

    修改和新增

--- Generic-Reflection-Homework/src/com/shaoxinyu/demo04/DemoTest04.java
+++ Generic-Reflection-Homework/src/com/shaoxinyu/demo04/DemoTest04.java
@@ -8,9 +8,11 @@ import java.util.Properties;
 public class DemoTest04 {
 
     public static void main(String[] args) {
+        //调方法获取properties对象
         Properties clazz = getClazz();
+        //通过properties对象的key值获取value值
         String className = clazz.getProperty("className");
-        System.out.println(className);
+        //System.out.println(className);
         try {
             Class<?> aClass = Class.forName(className);
             Method run = aClass.getDeclaredMethod("run");
@@ -21,7 +23,10 @@ public class DemoTest04 {
         }
     }
 
-
+    /**
+     * 读取文件中的数据
+     * @return 返回一个存有路径信息的properties对象
+     */
     public static Properties getClazz(){
         Properties properties = new Properties();
         InputStream input = DemoTest04.class.getClassLoader().getResourceAsStream("DemoClass.properties");
