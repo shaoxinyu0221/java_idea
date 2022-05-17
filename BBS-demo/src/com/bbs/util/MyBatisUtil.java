915df7d165724ba2e386f4d1c28261478c3a1757
shaoxinyu <2424590457@qq.com>
2022-05-17 20:29:27 +0800

    新增

---
+++ BBS-demo/src/com/bbs/util/MyBatisUtil.java
@@ -0,0 +1,42 @@
+package com.bbs.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisUtil {
+
+    private static SqlSessionFactory factory = null;
+
+    static {
+        try(InputStream input = Resources.getResourceAsStream("mybatis-config.xml");) {
+            SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+            factory = builder.build(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取一个SQLSession对象
+     * @return 返回一个sqlsession
+     */
+    public static SqlSession getSqlSession(){
+        return factory.openSession();
+    }
+
+    /**
+     * 关闭SQLSession
+     * @param sqlSession 要关闭的sqlsession对象
+     */
+    public static void close(SqlSession sqlSession){
+        if(sqlSession != null){
+            sqlSession.close();
+        }
+    }
+
+}
